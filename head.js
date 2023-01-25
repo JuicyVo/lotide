c7db34682be25b534b3039c7cbf6f4365a7abd8a
William <Williamtviet@hotmail.com>
2023-01-24 20:47:20 -0800

    added head.js

---
+++ head.js
@@ -0,0 +1,24 @@
+//returns the first element in a array
+
+const head = function (arrayProvided) {
+  //console.log (arrayProvided[0])
+  return (arrayProvided[0])
+} 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} != ${expected}`);
+  }
+};
+
+
+
+/* 
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head(["Hello"]), "Hello");
+assertEqual(head([]), "Hello");
+*/

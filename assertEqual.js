74d5b0e0582776a70a98bbc54376bf82353e4533
William <Williamtviet@hotmail.com>
2023-01-24 20:22:42 -0800

    added AssertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢" + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴" + "Assertion Failed: " + actual + " != " + expected);
+  }
+
+
+};
+
+
+//test
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
+
+//console.assert(1 === 1); // => nothing happens because true
+//console.assert(1 === 1.1);

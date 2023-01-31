a3a60a843d64bd1bb752c1e8e24b065ba33d01bf
William <Williamtviet@hotmail.com>
2023-01-30 18:35:03 -0800

    added letterPosition, countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,8 +1,46 @@
-
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`🟢🟢🟢 Assertion Passed ${actual} === ${expected}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed ${actual} != ${expected}`); 
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} != ${expected}`);
   }
 };
+
+const countLetters = function (word) {
+  const result = {};
+
+  for (const item of word) {
+    console.log(item);
+    if (result[item] === undefined) {
+      result[item] = 0;
+    }
+    result[item]++;
+  }
+  console.log(result);
+  return result;
+};
+
+countLetters("okk");
+// really gotta work on learning more on objects than arrays
+
+//for of loops works with strings
+
+/* 
+
+lightouse lab would return
+{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}
+
+
+
+*/
---
+++ letterPosition.js
@@ -0,0 +1,63 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function (firstArray, secondArray) {
+  //console.log (firstArray)
+  //console.log (secondArray)
+  if (firstArray.length !== secondArray.length) {
+    //console.log ("false, different lengths")
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        //console.log ("false, different values")
+        return false;
+      } else {
+        //console.log ("true")
+      }
+    }
+    return true;
+  }
+};
+
+const assertArrayEquals = function (firstArray, secondArray) {
+  console.log(eqArrays(firstArray, secondArray));
+  let arrayAssert = eqArrays(firstArray, secondArray);
+  if (arrayAssert === true) {
+    console.log(`🟢🟢🟢 Assertion Passed ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed ${firstArray} !== ${secondArray}`);
+  }
+};
+
+const letterPositions = function (item) {
+  const result = {};
+  for (let i = 0; i <= item.length; i++) {
+    let element = item[i];
+    if (element !== " ") {
+      if (result[element] === undefined) {
+        result[element] = [i];
+      } else if (result[element] !== undefined) {
+        result[element].push(i);
+      }
+    }
+  }
+  //result[element]++; //this probably not right so keep it off
+  //console.log(counter)
+
+  // logic to update results here
+  console.log(result);
+  return result;
+};
+
+letterPositions("hello");
+letterPositions("lighthouse in the house");
+//assertArrayEquals(letterPositions("hello"), [1]);
+
+//assertArrayEquals(letterPositions ("hello"))
+//should = h:1, e:2, l:3, 4, : o:5
